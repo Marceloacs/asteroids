d37f4c9809194355d62b4e37075768dace60cd22
Marcelo AC <marcelo.ac91@icloud.com>
2025-02-12 11:18:25 +0100

    fix

--- server-app/src/integrations/nasa/get-asteroids.ts
+++ server-app/src/integrations/nasa/get-asteroids.ts
@@ -6,9 +6,7 @@ const NASA_API_KEY = process.env.NASA_API_KEY || "DEMO_KEY";
 export async function getNasaAsteroids(payload: GetNasaAsteroidsPayload): Promise<GetNasaAsteroidsResponse> {
     try {
         const response = await fetch(`https://api.nasa.gov/neo/rest/v1/feed?start_date=${payload.startDate}&end_date=${payload.endDate}&api_key=${NASA_API_KEY}`);
-        const data = await response.json() as GetNasaAsteroidsResponse;
-        console.log("data", data);
-        return data;
+        return await response.json() as GetNasaAsteroidsResponse;
         // FAKE DATA
         // return await getFakeResponse();
     } catch (error) {
